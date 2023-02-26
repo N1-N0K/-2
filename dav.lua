73bb70e7a0c6cf24cd78c6f8b622a69deb7f0ca5
N2-NOK <kvijinadzenino11@gmail.com>
2023-02-26 18:04:37 +0400

    first commit

---
+++ dav.lua
@@ -0,0 +1,209 @@
+a = 1
+b = 2
+c = 3
+d = 4
+e = 5
+f = 6
+g = 7
+h = 8
+i = 9
+j = 10
+k = 11
+l = 12
+m = 13
+n = 14
+o = 15
+p = 16
+q = 17
+r = 18
+s = 19
+t = 20
+u = 21
+v = 22
+w = 23
+x = 24
+y = 25
+z = 26
+ab = 27
+ac =28
+aa = 29
+ad = 30
+ae = 31
+ag = 32
+ah = 33
+aj = 34
+ak = 35
+al = 36
+am = 37
+an = 38
+ao = 39
+ap = 40
+aq= 41
+ar = 42
+as = 43
+at = 44
+au = 45
+av = 46
+aw = 47
+ax = 48
+ay = 49
+az = 50
+ba = 51
+bb = 52
+bc = 53
+bd = 54
+be = 55
+bf = 56
+bg = 57
+bh = 58
+bi = 59
+bj = 60
+bk = 61
+bl = 62
+bm = 63
+bn = 64
+bo = 65
+bp = 66
+bq = 67
+br = 68
+bs = 69
+bt = 70
+bu = 71
+bv = 72
+bw = 73
+bx = 74
+by = 75
+bz = 76
+ca = 77
+cb = 78
+cc = 79
+dd = 80
+ee = 81
+ff = 82
+gg = 83
+hh = 84
+ii = 85
+jj = 86
+kk = 87
+ll = 88
+mm = 89
+nn = 90
+oo = 91
+pp = 92
+qq = 93
+rr = 94
+ss = 95
+tt = 96
+uu = 97
+vv = 98
+ww = 99
+xx = 100  
+
+
+
+
+
+
+
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)
+print(g)
+print(h)
+print(i)
+print(j)
+print(k)
+print(l)
+print(m)
+print(n)
+print(o)
+print(p)
+print(q)
+print(r)
+print(s)
+print(t)
+print(u)
+print(v)
+print(w)
+print(x)
+print(y)
+print(z)
+print(ab)
+print(ac)
+print(aa)
+print(ad)
+print(ae)
+print(af)
+print(ag)
+print(ah)
+print(ai)
+print(aj)
+print(ak)
+print(al)
+print(am)
+print(an)
+print(ao)
+print(ap)
+print(aq)
+print(ar)
+print(as)
+print(at)
+print(au)
+print(av)
+print(aw)
+print(ax)
+print(ay)
+print(az)
+print(ba)
+print(bb)
+print(bc)
+print(bd)
+print(be)
+print(bh)
+print(bi)
+print(bj)
+print(bk)
+print(bl)
+print(bm)
+print(bn)
+print(bo)
+print(bp)
+print(bq)
+print(br)
+print(bs)
+print(bt)
+print(bu)
+print(bv)
+print(bw)
+print(bx)
+print(by)
+print(bz)
+print(ca)
+print(cb)
+print(cc)
+print(dd)
+print(ee)
+print(ff)
+print(gg)
+print(hh)
+print(ii)
+print(jj)
+print(kk)
+print(ll)
+print(mm)
+print(nn)
+print(oo)
+print(pp)
+print(qq)
+print(rr)
+print(ss)
+print(tt)
+print(uu)
+print(vv)
+print(ww)
+print(xx)
+
+
