51e2eb3f28a07424a3e4bf4cdcdd0858b51eb5ec
N2-NOK <kvijinadzenino11@gmail.com>
2023-03-02 10:54:45 +0400

    commit

---
+++ 35.lua
@@ -0,0 +1,29 @@
+n = 0
+
+
+for i = 20, 35, 1 do
+   if i%5 == 0 then
+   n = n + i 
+end
+end
+
+print(n)
+
+
+for i = 20, 35, 1 do
+   if i%7 == 0 then
+   n = n + i
+ 
+end
+end
+
+print(n)
+
+
+for i = 20, 35, 1 do
+   if i%35 == 0 then
+   n = n - i
+end
+end
+
+print(n)
